cbb47f4fab7ebe1757eb54b932ba99b7ce00bf06
viruop <virajrai706@gmail.com>
2024-03-08 17:55:58 +0530

    login:wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
 import "./App.css";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/second-page" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
