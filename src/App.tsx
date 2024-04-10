7cb7ca1580e1f4a398a0802a27fcf4ff29855903
viruop <virajrai706@gmail.com>
2024-04-10 10:48:21 +0530

    commented post login page

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      {/* <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} /> */}
     </Routes>
   );
 };
